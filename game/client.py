16ce7e2defdb68f8a42ceae0439cffb8036f4767
MisterAV009 <amir.vosidov05@gmail.com>
2025-01-02 18:40:31 +0300

    add sql_table

--- game/client.py
+++ game/client.py
@@ -1,5 +1,6 @@
 import socket
 import pygame
+import math
 
 pygame.init()
 
@@ -7,6 +8,13 @@ sockets = socket.socket(socket.AF_INET,socket.SOCK_STREAM) # выбираем с
 sockets.setsockopt(socket.IPPROTO_TCP,socket.TCP_NODELAY,1) # откл. пакетирование,чтобы не портить фпс
 sockets.connect(("localhost",10000)) # привязываем айпи адрес и порт
 
+def draw_text(x,y,r,text,color):
+    font = pygame.font.Font(None,r)
+    text = font.render(text,True,color)
+    rect = text.get_rect(center=(x,y))
+    screen.blit(text,rect)
+
+name = 'bob'
 
 WIDTH = 800
 HEIGHT = 600
@@ -25,14 +33,22 @@ while run:
             pos = pygame.mouse.get_pos()
 
             vector = pos[0] - CC[0],pos[1] - CC[1]
+            lenv = math.sqrt(vector[0] ** 2 + vector[1] ** 2)
+            vector = vector[0] / lenv, vector[1] / lenv
+            if lenv <= radius:
+                vector = (0,0)
             if vector != old:
                 old = vector
                 msg = f'<{vector[0]},{vector[1]}>'
                 sockets.send(msg.encode())
+            print(vector)
     data = sockets.recv(1024).decode()
     print(data)
     screen.fill("gray")
     pygame.draw.circle(screen, (255, 0, 0), CC, radius)
+    draw_text(CC[0],CC[1],radius // 2,name,'blue')
     pygame.display.update()
+
+
 
 pygame.quit()
--- game/server.py
+++ game/server.py
@@ -1,4 +1,15 @@
 import socket
+import time
+import psycopg2
+from sqlalchemy import create_engine,Column,String,Integer
+from sqlalchemy.orm import declarative_base,sessionmaker
+
+
+engine = create_engine('postgresql+psycopg2://postgres:1@localhost/bacterias')
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
+s = Session()
+
 
 main_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM) # выбираем семейство адресов IPv4,тип сокета TCP
 main_socket.setsockopt(socket.IPPROTO_TCP,socket.TCP_NODELAY,1) # откл. пакетирование,чтобы не портить фпс
@@ -6,6 +17,30 @@ main_socket.bind(("localhost",10000)) # привязываем айпи адре
 main_socket.setblocking(False) # непрерывность, сервер не ждет ответ от клиента
 main_socket.listen(5) # прослушка 5 одновременных соединений
 print('cокет подключен')
+
+
+class Player(Base):
+    __tablename__ = "gamers"
+    id = Column(Integer, primari_key=True, autoincrement=True)
+    name = Column(String(250))
+    address = Column(String)
+    x = Column(Integer, defaulnt=500)
+    y = Column(Integer, defaulnt=500)
+    site = Column(Integer, defaulnt=50)
+    errors = Column(Integer, defaulnt=0)
+    abs_speed = Column(Integer, defaulnt=1)
+    speed_x = Column(Integer, defaulnt=0)
+    speed_y = Column(Integer, defaulnt=0)
+    color = Column(String(250), default='red')
+    w_vision = Column(Integer, defaulnt=800)
+    h_vision = Column(Integer, defaulnt=600)
+
+    def __init__(self, name, address):
+        self.name = name
+        self.address = address
+
+
+Base.metadata.create_all(engine)
 players = []
 
 while True:
@@ -29,3 +64,6 @@ while True:
             players.remove(id)
             id.close()
             print('сокет закрыт')
+
+
+    time.sleep(1)
